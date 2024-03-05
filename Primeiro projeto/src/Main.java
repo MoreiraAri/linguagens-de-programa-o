ad56e88a391bfcc7679a65021df37c0e11693141
MoreiraAri <144627734+MoreiraAri@users.noreply.github.com>
2024-03-05 20:28:49 -0300

    Add files via upload
    Primeiro projeto JAVA

---
+++ Primeiro projeto/src/Main.java
@@ -0,0 +1,38 @@
+
+
+import java.io.IOException;
+public class Main {
+    public static void main (String[] args) throws IOException {
+        System.out.println("Olá, mundo!");
+        char ch;
+        ch = 'X';
+        System.out.println("ch contains" + ch);
+        ch++;//incrementa ch
+        System.out.println("ch is now" + ch);
+        ch = 90; // dá a ch o valor de Z
+        System.out.println("ch is now" +ch);
+
+        byte tipoByte = 127;
+        short tiposhort = 32767;
+        char tipochar = 'C';
+        float tipoFloat = 2.6f;
+        double tipoDouble = 3.59;
+        int tipoInt = 2142551546;
+        long tipoLong = 92223564235697L;
+        boolean tipoBooleano = true;
+        System.out.println("Valor do tipoByte = " +tipoByte);
+        System.out.println("Valor do tipoShort = " +tiposhort);
+        System.out.println("Valor do tipoChar = " +tipochar);
+        System.out.println("Valor do tipoFloat = " +tipoFloat);
+        System.out.println("Valor do tipoDouble = " +tipoDouble);
+        System.out.println("Valor do tipoInt = " +tipoInt);
+        System.out.println("Valor do tipoLong = " +tipoLong);
+        System.out.println("Valor do tipoBooleano = " +tipoBooleano);
+
+
+
+
+
+    }
+}
+
